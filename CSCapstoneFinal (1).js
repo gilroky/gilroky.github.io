aca07f2bda7bc942c769dad9156f5f3225775b1b
gilroky <57300892+gilroky@users.noreply.github.com>
2019-12-17 00:49:29 -0500

    Add files via upload

---
+++ CSCapstoneFinal (1).js
@@ -0,0 +1,71 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.createCollection("customers", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("CustomerInformation");
+  var myobj = [
+    { name: 'Kyle', address: '313 Madison'},
+    { name: 'Ryan', address: 'Mississippi Road'},
+    { name: 'Bob', address: 'Apple St 652'},
+    { name: 'Bill', address: 'Mountain 21'},
+    { name: 'Brady', address: 'Valley 345'},
+		{ name: 'Tom', address: 'Valley 342'},
+		{ name: 'Harry', address: 'Valley 340'},
+		{ name: 'Minerva', address: 'Valley Road'},
+  ];
+  dbo.collection("customers").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("CustomerInformation");
+  dbo.collection("customers").findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result.name);
+    db.close();
+  });
+});
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("CustomerInformation");
+  var mysort = { name: 1 };
+  dbo.collection("customers").find().sort(mysort).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    db.close();
+  });
+});
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("CustomerInformation");
+  var myquery = { address: 'Hogwarts Lane' };
+  dbo.collection("customers").deleteOne(myquery, function(err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    db.close();
+  });
+});
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("CustomerInformation");
+  var myquery = { address: "Valley 345" };
+  var newvalues = { $set: {name: "Mickey", address: "Canyon Road" } };
+  dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+    db.close();
+  });
+});
